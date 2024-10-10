592bc1b937e18a0642081e679daf0062bc4efa6e
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-10-10 11:05:04 -0300

    tela conta terminada

--- src/screens/Conta.js
+++ src/screens/Conta.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import Button from "../components/Button.js"; 
 import '../styles/Conta.css';
+import avatarFoto from '../img/user.png'
 
 const Conta = () => {
   const [userLogado, setUserLogado] = useState(null);
@@ -28,19 +29,19 @@ const Conta = () => {
   };
 
   return (
-    <div className="container">
-      <div className="bg">
+    <div className="container-conta">
+      
         <div className="centered-container">
-          <h1 className="titulo">Sua conta</h1>
+          <h1 className="titulo-conta">Sua conta</h1>
 
-          <img className="profile-image" src="/path/to/profile-image.jpg" alt="Avatar" />
-          <p className="name"><b>Nome de usuário:</b> {userLogado?.name}</p>
-          <p className="name"><b>E-mail:</b> {userLogado?.email}</p>
+          <img className="profile-image" src={avatarFoto} alt="Avatar" />
+          <p className="name"><b className="labelConta">Nome de usuário:</b> {userLogado?.name}</p>
+          <p className="name"><b className="labelConta">E-mail:</b> {userLogado?.email}</p>
         </div>
 
         <Button style={{ alignSelf: "center" }} title="Editar" onClick={() => navigate("/editar-perfil")} />
         <Button style={{ alignSelf: "center" }} title="Sair" onClick={handleLogout} />
-      </div>
+      
     </div>
   );
 };
