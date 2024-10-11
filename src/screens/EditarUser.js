68d5fb22e35123767b33841315e28ecc6641ce22
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-10-11 10:43:57 -0300

    finish screen receita

--- src/screens/EditarUser.js
+++ src/screens/EditarUser.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom"; 
-import Button from "../components/Button.js"; 
+import { useLocation } from "react-router-dom";
+import Button from "../components/Button.js";
 import authFetch from "../helpers/authFetch";
-import "../styles/EditarUser.css"; 
-import { useNavigate } from 'react-router-dom'; // Importando useNavigate
+import "../styles/EditarUser.css";
+import { useNavigate } from "react-router-dom"; // Importando useNavigate
 
 const EditarUser = () => {
   const navigate = useNavigate(); // Usando useNavigate
   const location = useLocation();
-  const { userLogado } = location.state || {}; 
+  const { userLogado } = location.state || {};
 
   const [txtName, setTxtName] = useState(userLogado.name);
   const [txtEmail, setTxtEmail] = useState(userLogado.email);
@@ -42,7 +42,7 @@ const EditarUser = () => {
 
       const data = await result.json();
       if (data?.success) {
-        localStorage.setItem('userLogged', JSON.stringify(data.user)); // Usando localStorage
+        localStorage.setItem("userLogged", JSON.stringify(data.user)); // Usando localStorage
         navigate(-1); // Usando navigate para voltar
       } else {
         alert(data.error);
@@ -55,69 +55,70 @@ const EditarUser = () => {
 
   const removeUser = async () => {
     try {
-      const result = await authFetch(`https://backcooking.onrender.com/user/${userLogado.id}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
+      const result = await authFetch(
+        `https://backcooking.onrender.com/user/${userLogado.id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-      });
+      );
 
       if (!result.ok) {
         const dataError = await result.json();
         if (dataError?.error && dataError?.code === "logout") {
-          alert('Sessão expirada!');
-          navigate('/login'); // Usando navigate para redirecionar
+          alert("Sessão expirada!");
+          navigate("/login"); // Usando navigate para redirecionar
           return;
         }
       }
 
       const data = await result.json();
       if (data?.success) {
-        localStorage.removeItem('userLogged'); // Remove o usuário do localStorage
-        navigate('/splash'); // Usando navigate para redirecionar
+        localStorage.removeItem("userLogged"); // Remove o usuário do localStorage
+        navigate("/splash"); // Usando navigate para redirecionar
       } else {
         alert(data.error);
       }
     } catch (error) {
-      console.log('Error removeUser ' + error.message);
+      console.log("Error removeUser " + error.message);
       alert(error.message);
     }
   };
 
   return (
-<>
+    <>
       <div className="container-editar-user">
-      <div className="centered-container">
-        <h1 className="titulo-editar-user">Editar</h1>
-        <div className="form">
-          <input
-            className="input"
-            placeholder="Nome..."
-            onChange={(e) => setTxtName(e.target.value)}
-            value={txtName}
-          />
-          <input
-            className="input"
-            placeholder="Email..."
-            onChange={(e) => setTxtEmail(e.target.value)}
-            value={txtEmail}
-          />
-          <input
-            className="input"
-            placeholder="Avatar..."
-            onChange={(e) => setTxtAvatar(e.target.value)}
-            value={txtAvatar}
-          />
+        <div className="centered-container">
+          <h1 className="titulo-editar-user">Editar</h1>
+          <div className="form">
+            <input
+              className="input"
+              placeholder="Nome..."
+              onChange={(e) => setTxtName(e.target.value)}
+              value={txtName}
+            />
+            <input
+              className="input"
+              placeholder="Email..."
+              onChange={(e) => setTxtEmail(e.target.value)}
+              value={txtEmail}
+            />
+            <input
+              className="input"
+              placeholder="Avatar..."
+              onChange={(e) => setTxtAvatar(e.target.value)}
+              value={txtAvatar}
+            />
+          </div>
         </div>
-        </div>
-        
+
         <Button title="Cancelar" onClick={() => navigate("/conta")} />
         <Button title="Salvar" onClick={editUser} />
         <Button title="Apagar conta" onClick={removeUser} />
       </div>
-      
-      
-   </>
+    </>
   );
 };
 
